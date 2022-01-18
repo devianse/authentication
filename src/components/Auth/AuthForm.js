b8206df7ab44ae5a99ea569cf45aad7f263a71b2
Михаил Харлов <p4kc0dn43eml1@gmail.com>
2022-01-18 18:17:24 +0300

    adding basic user feedback if error on creating acc

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,6 +19,7 @@ const AuthForm = () => {
     const enteredPassowrd = passwordInputRef.current.value;
 
     //validation ?
+    setIsLoading(true);
 
     if (isLogin) {
     } else {
@@ -33,12 +35,16 @@ const AuthForm = () => {
           headers: { "Content-Type": "application/json" },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
           //...
         } else {
           return res.json().then((data) => {
-            //show an error modal
-            console.log(data);
+            let errorMessage = "Authentication failed";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -63,7 +69,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
