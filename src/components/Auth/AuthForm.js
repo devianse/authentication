70f94230d318c42565f88ae116d83b024cfc484a
Михаил Харлов <p4kc0dn43eml1@gmail.com>
2022-01-18 19:22:15 +0300

    added useContext to manage state IsLoggedIn or not + dynamically render nav based on that

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 import key from "../../env_var";
@@ -6,6 +7,9 @@ import key from "../../env_var";
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -52,7 +56,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err);
