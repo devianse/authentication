7db191370766962a869c8bc483d05c7550cba949
Михаил Харлов <p4kc0dn43eml1@gmail.com>
2022-01-18 18:39:53 +0300

    added user login fetch, restructured response logic a bit, now we have a token to work with

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -16,39 +16,47 @@ const AuthForm = () => {
   const submitHandler = (event) => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
-    const enteredPassowrd = passwordInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
     //validation ?
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`;
     } else {
-      fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassowrd,
-            returnSecureToken: true,
-          }),
-          headers: { "Content-Type": "application/json" },
-        }
-      ).then((res) => {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          //...
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed";
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err);
       });
-    }
   };
 
   return (
